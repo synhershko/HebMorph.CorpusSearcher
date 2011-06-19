ef33ffba9f87963f35579e56b41f582a442a2110
synhershko <itamar@code972.com>
2011-06-19 18:04:58 +0300

    Forgotten file

---
+++ HebMorph.CorpusSearcher/Helpers/StringExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HebMorph.CorpusSearcher.Helpers
+{
+	public static class StringExtensions
+	{
+		public static string UrlEncode(this string val)
+		{
+			return HttpUtility.UrlEncode(val);
+		}
+
+		public static string UrlDecode(this string val)
+		{
+			return HttpUtility.UrlDecode(val);
+		}
+
+		public static string HtmlEncode(this string val)
+		{
+			return HttpUtility.HtmlEncode(val);
+		}
+
+		public static string HtmlDecode(this string val)
+		{
+			return HttpUtility.HtmlDecode(val);
+		}
+	}
+}
